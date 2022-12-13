e8d1ef260773a57775aa7d9b238499b1106ffcd1
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-12-13 15:57:04 -0300

    finaliza requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Conteudo from './components/Conteudo';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes - início</p>);
+    return (
+      <BrowserRouter>
+        <Conteudo />
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import Search from '../pages/Search';
+
+class Conteudo extends React.Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/album/:id" component={ Album } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Conteudo;
