88dfd794ce7f4dbdef07e4e9da34340d5656d404
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-12-15 10:01:00 -0300

    refatora o requisito 6

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -4,44 +4,34 @@ import PropTypes from 'prop-types';
 
 class AlbumCard extends React.Component {
   render() {
-    const { albuns, artist } = this.props;
-    console.log(albuns);
+    const { album: {
+      collectionName,
+      artistName,
+      artworkUrl100,
+      collectionId,
+    } } = this.props;
     return (
-      <div>
-        {
-          albuns.length === 0
-            ? <p>Nenhum álbum foi encontrado</p>
-            : (
-              <section className="returnAlbunsAPI">
-                <p>{`Resultado de álbuns de: ${artist}`}</p>
-                <div className="returnAlbuns">
-                  <ul className="listaDeAlbuns">
-                    {albuns.map(({ collectionId, collectionName }) => (
-                      <li key={ collectionId }>
-                        <Link
-                          to={ `/album/${collectionId}` }
-                          data-testid={ `link-to-album-${collectionId}` }
-                        >
-                          {collectionName}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </section>
-            )
-        }
-      </div>
+      <>
+        <img src={ artworkUrl100 } alt={ `Capa do álbum ${collectionName}` } />
+        <Link
+          to={ `/album/${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        >
+          { collectionName }
+        </Link>
+        <p>{ artistName }</p>
+      </>
     );
   }
 }
 
 AlbumCard.propTypes = {
-  albuns: PropTypes.arrayOf(PropTypes.shape({
+  album: PropTypes.shape({
     collectionId: PropTypes.number,
     collectionName: PropTypes.string,
-  })).isRequired,
-  artist: PropTypes.string.isRequired,
+    artistName: PropTypes.string,
+    artworkUrl100: PropTypes.string,
+  }).isRequired,
 };
 
 export default AlbumCard;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Carregando from '../components/Carregando';
 import AlbumCard from '../components/AlbumCard';
+// import ListAlbuns from '../components/ListAlbuns';
 
 class Search extends React.Component {
   constructor() {
@@ -19,7 +20,7 @@ class Search extends React.Component {
     this.validationSearch = this.validationSearch.bind(this);
     this.searchMusic = this.searchMusic.bind(this);
     this.htmlElements = this.htmlElements.bind(this);
-    // this.renderElements = this.renderElements.bind(this);
+    this.renderElements = this.renderElements.bind(this);
     this.callAPI = this.callAPI.bind(this);
   }
 
@@ -57,33 +58,9 @@ class Search extends React.Component {
     );
   }
 
-  // renderElements() {
-  //   const { albuns, artist } = this.state;
-  //   console.log('albuns', albuns)
-  //   if (albuns.length = 0) {
-  //     return (<p>Nenhum álbum foi encontrado</p>);
-  //   } else {
-  //     console.log('else', albuns)
-  //     return (
-  //       <section className="returnAlbunsAPI">
-  //       <p>Resultado de álbuns de: {artist}</p>
-  //       <div className="returnAlbuns">
-  //         <ul className="listaDeAlbuns">
-  //           {albuns.map((album) => (
-  //             <li key={album.collectionId}>
-  //               {album.collectionId}
-  //             </li>
-  //           ))}
-  //         </ul>
-  //       </div>
-  //       </section>
-  //     );
-  //   }
-  // }
-
   searchMusic() {
     this.setState(
-      { isSearching: true },
+      { isSearching: true }, // aqui ele já renderiza de novo antes de eecutar a função?
       this.callAPI,
     );
   }
@@ -92,12 +69,40 @@ class Search extends React.Component {
     const { valueInput } = this.state;
     const albunsAPI = await searchAlbumsAPI(valueInput);
     this.setState(
-      { albuns: albunsAPI, isSearching: false, artist: valueInput, valueInput: '' },
+      {
+        albuns: albunsAPI,
+        isSearching: false,
+        artist: valueInput,
+        valueInput: '',
+        isButtonDisabled: true,
+      },
+    );
+  }
+
+  renderElements() {
+    const { albuns, artist } = this.state;
+    // console.log('albuns', albuns);
+    if (albuns.length === 0) {
+      return (<h2>Nenhum álbum foi encontrado</h2>);
+    } return (
+      <section className="returnAlbunsAPI">
+        <p>{`Resultado de álbuns de: ${artist}`}</p>
+        <div className="returnAlbuns">
+          <ul className="listOfAlbuns">
+            {albuns.map((album) => (
+              <li className="albunsSearch" key={ album.collectionId }>
+                <AlbumCard album={ album } />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     );
   }
 
   render() {
-    const { isSearching, albuns, artist } = this.state;
+    const { isSearching, artist } = this.state;
+    // const { isSearching, albuns, artist } = this.state;
     // const teste = searchAlbumsAPI('pink');
     // console.log('albuns Render', albuns)
 
@@ -105,8 +110,9 @@ class Search extends React.Component {
       <div data-testid="page-search">
         <Header />
         {isSearching ? <Carregando /> : this.htmlElements()}
-        {/* {artist.length > 0 && this.renderElements()} */}
-        {artist.length > 0 && <AlbumCard albuns={ albuns } artist={ artist } />}
+        {artist.length > 0 && this.renderElements()}
+        {/* usei a expressão abaixo quando não estava conseguindo renderizar aqui */}
+        {/* {artist.length > 0 && <ListAlbuns albuns={ albuns } artist={ artist } />} */}
       </div>
     );
   }
