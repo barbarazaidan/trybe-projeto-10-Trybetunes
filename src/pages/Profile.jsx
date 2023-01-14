bb220e6f9d7ba4a593aab9fee1284d72442d94df
Bárbara <babivaldez@gmail.com>
2023-01-14 17:43:31 -0300

    finaliza requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,60 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
+import { Link } from 'react-router-dom';
 
 class Profile extends React.Component {
+  state = {
+    isCallingApi: true,
+    description: '',
+    email: '',
+    image: '',
+    name: '',
+  };
+
+  componentDidMount() {
+    this.userInfo();
+  }
+
+  userInfo = async () => {
+    const user = await getUser();
+    // console.log(user); // retorna um objeto
+    const { description, email, image, name } = user;
+    this.setState({
+      description,
+      email,
+      image,
+      name,
+      isCallingApi: false,
+    });
+  };
+
   render() {
+    const { isCallingApi, description, email, image, name } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>perfil</p>
+        { isCallingApi
+          ? <Carregando />
+          : (
+            <section>
+              <div>
+                <img
+                  data-testid="profile-image"
+                  src={ image }
+                  alt="foto do usuário"
+                />
+                <Link to="/profile/edit">Editar perfil</Link>
+              </div>
+              <h3>Nome</h3>
+              <p>{ name }</p>
+              <h3>Email</h3>
+              <p>{ email }</p>
+              <h3>Descrição</h3>
+              <p>{ description }</p>
+            </section>
+          )}
       </div>
     );
   }
